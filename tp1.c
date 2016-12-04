65040f843b7ab20fc7fdbd19b9afb9b29dd92ff3
Your Name <you@example.com>
2016-12-04 11:38:21 +0100

    correction de recherche de mot

--- tp1.c
+++ tp1.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int chercheMot(char *tableau, char* nom_fichier)
+void chercheMot(char *mot, char* nom_fichier)
 {
-    char car = 0;   //Contient temporairement chaque caractère du fichier dico.txt
+    char car = 0;  
     int i = 0, ligne=1, colonne=1;
 	printf("\n>>> Dans le fichier %s \n", nom_fichier);
     FILE* fichier = fopen(nom_fichier, "r");
     while(car != EOF)
     {
-        car = fgetc(fichier);   //On lit un caractère de dico.txt
+        car = fgetc(fichier);   
         colonne++;
 	if (car == '\n'){
 		ligne++;
 		colonne=1;
  		
 	}
-        if(tableau[i] == car)   //Si le 1er caractère correspond, on augmente i pour tester le second caractère au tour suivant
+        if(mot[i] == car)  
         {
             i++;
-            if(tableau[i] == '\0' && fgetc(fichier) == '\n') //Si cette condition est vraie, c'est que la chaine est déja dans le dico
+            if(mot[i] == '\0' && i == strlen(mot))
             {
-                printf("ligne %d colonne %ld dans le fichier %s \n", ligne, colonne-strlen(tableau), nom_fichier);
+                printf("ligne %d colonne %ld  \n", ligne, colonne-strlen(mot));
 		//return 1;
 		
             }
@@ -33,12 +33,11 @@ int chercheMot(char *tableau, char* nom_fichier)
         }
     } 
     fclose(fichier); 
-    return 0;   //Si on a rien trouvé, on retourne 0
+     
 }
   void main(int argc,char* argv[])
   {
-    char cmd[20]; FILE* fof; FILE* fp; char lin[100], lin2[100];int i;char chemin_fichier[100];
-  //  char *lin3 = malloc(100 * sizeof(char));
+    char cmd[20]; FILE* fof; char lin[100];int i;char chemin_fichier[100];
     if (argc < 2)
     {
       printf("nombre de parametres insuffisant");
@@ -57,7 +56,7 @@ int chercheMot(char *tableau, char* nom_fichier)
           strncpy(lin3,lin, strlen(lin)-1);
           strcpy(chemin_fichier, argv[1]);
           strcat(chemin_fichier, lin3);
-          int o = chercheMot(argv[i], chemin_fichier );
+          chercheMot(argv[i], chemin_fichier );
           free(lin3);	  
 	}
         fclose(fof);
